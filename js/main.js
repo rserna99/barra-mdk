d3bdc0a850d1a71a5a8bd36ddf8a45334dcfdf41
rserna99 <rogerserna99@gmail.com>
2026-05-11 19:04:22 +0200

    debug

--- js/main.js
+++ js/main.js
@@ -30,6 +30,7 @@ $(document).ready(function(){
                     </li>
                 `;
                 console.log(card);
+                console.log(contenidor);
                 
                 contenidor.append(card);
 
@@ -85,39 +86,39 @@ $(document).ready(function(){
 
 
     // Elements
-    let btn_birra = $('#birra');
-    let recompte_birra = $('#recompte_birra');
-    let total_birra = $('#total_birra');
+    // let btn_birra = $('#birra');
+    // let recompte_birra = $('#recompte_birra');
+    // let total_birra = $('#total_birra');
     //let preu_birra = 2;
 
-    let btn_convinat = $('#convinat');
-    let recompte_convinat = $('#recompte_convinat');
-    let total_convinat = $('#total_convinat');
+    // let btn_convinat = $('#convinat');
+    // let recompte_convinat = $('#recompte_convinat');
+    // let total_convinat = $('#total_convinat');
     //let preu_convinat = 5;
 
-    let btn_xarrup = $('#xarrup');
-    let recompte_xarrup = $('#recompte_xarrup');
-    let total_xarrup = $('#total_xarrup');
+    // let btn_xarrup = $('#xarrup');
+    // let recompte_xarrup = $('#recompte_xarrup');
+    // let total_xarrup = $('#total_xarrup');
     //let preu_xarrup = 2;
 
-    let btn_vermut = $('#vermut');
-    let recompte_vermut = $('#recompte_vermut');
-    let total_vermut = $('#total_vermut');
+    // let btn_vermut = $('#vermut');
+    // let recompte_vermut = $('#recompte_vermut');
+    // let total_vermut = $('#total_vermut');
     //let preu_vermut = 4;
 
-    let btn_calimotxo = $('#calimotxo');
-    let recompte_calimotxo = $('#recompte_calimotxo');
-    let total_calimotxo = $('#total_calimotxo');
+    // let btn_calimotxo = $('#calimotxo');
+    // let recompte_calimotxo = $('#recompte_calimotxo');
+    // let total_calimotxo = $('#total_calimotxo');
     //let preu_calimotxo = 4;
 
-    let btn_refresc = $('#refresc');
-    let recompte_refresc = $('#recompte_refresc');
-    let total_refresc = $('#total_refresc');
+    // let btn_refresc = $('#refresc');
+    // let recompte_refresc = $('#recompte_refresc');
+    // let total_refresc = $('#total_refresc');
     //let preu_refresc = 2;
 
-    let btn_got = $('#got');
-    let recompte_got = $('#recompte_got');
-    let total_got = $('#total_got');
+    // let btn_got = $('#got');
+    // let recompte_got = $('#recompte_got');
+    // let total_got = $('#total_got');
     //let preu_got = 1;
 
     let total = $('#total');
